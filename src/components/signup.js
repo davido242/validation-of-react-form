de62c64384e3b1d1dccd9e9fd5ad600e0d921787
Monday_David_Sarka <david_md@hamari.agency>
2022-06-17 23:55:45 +0100

    added schema page to src directory

---
+++ src/components/signup.js
@@ -0,0 +1,111 @@
+import { useFormik } from "formik";
+import { basicSchema } from "../schema";
+const onSubmit = async (values, actions) => {
+  console.log(values);
+  console.log(actions);
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  actions.resetForm();
+};
+const SignUp = () => {
+  const {
+    values,
+    errors,
+    touched,
+    isSubmitting,
+    handleBlur,
+    handleChange,
+    handleSubmit,
+  } = useFormik({
+    initialValues: {
+      firstname: "",
+      lastname: "",
+      email: "",
+      age: "",
+      password: "",
+      confirmPassword: "",
+    },
+    validationSchema: basicSchema,
+    onSubmit,
+  });
+  console.log(errors);
+  return (
+    <form onSubmit={handleSubmit} autoComplete="off">
+        <label htmlFor="firstname">First Name</label>
+      <input
+        value={values.firstname}
+        onChange={handleChange}
+        id="firstname"
+        type="firstname"
+        placeholder="Enter First Name"
+        onBlur={handleBlur}
+        className={errors.firstname && touched.firstname ? "input-error" : ""}
+      />
+      {errors.firstname && touched.firstname && <p className="error">{errors.firstname}</p>}
+      <label htmlFor="lastname">Last Name</label>
+      <input
+        value={values.lastname}
+        onChange={handleChange}
+        id="lastname"
+        type="lastname"
+        placeholder="Enter Last Name"
+        onBlur={handleBlur}
+        // className={errors.email && touched.email ? "input-error" : ""}
+      />
+      {errors.lastname && touched.lastname && <p className="error">{errors.lastname}</p>}
+      <label htmlFor="email">Email</label>
+      <input
+        value={values.email}
+        onChange={handleChange}
+        id="email"
+        type="email"
+        placeholder="Enter your email"
+        onBlur={handleBlur}
+        className={errors.email && touched.email ? "input-error" : ""}
+      />
+      {errors.email && touched.email && <p className="error">{errors.email}</p>}
+      <label htmlFor="age">Age</label>
+      <input
+        id="age"
+        type="number"
+        placeholder="Enter your age"
+        value={values.age}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        className={errors.age && touched.age ? "input-error" : ""}
+      />
+      {errors.age && touched.age && <p className="error">{errors.age}</p>}
+      <label htmlFor="password">Password</label>
+      <input
+        id="password"
+        type="password"
+        placeholder="Enter your password"
+        value={values.password}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        className={errors.password && touched.password ? "input-error" : ""}
+      />
+      {errors.password && touched.password && (
+        <p className="error">{errors.password}</p>
+      )}
+      <label htmlFor="confirmPassword">Confirm Password</label>
+      <input
+        id="confirmPassword"
+        type="password"
+        placeholder="Confirm password"
+        value={values.confirmPassword}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        className={
+          errors.confirmPassword && touched.confirmPassword ? "input-error" : ""
+        }
+      />
+      {errors.confirmPassword && touched.confirmPassword && (
+        <p className="error">{errors.confirmPassword}</p>
+      )}
+      <button disabled={isSubmitting} type="submit">
+        Submit
+      </button>
+    </form>
+  );
+};
+export default SignUp;
---
+++ src/schema.js
@@ -0,0 +1,38 @@
+import * as yup from "yup";
+
+const passwordRules = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,}$/;
+// min 5 characters, 1 upper case letter, 1 lower case letter, 1 numeric digit.
+
+export const basicSchema = yup.object().shape({
+    firstname: yup.string()
+    .max(15, 'Must be 15 characters or less')
+    .required('Required'),
+  lastname: yup.string()
+    .max(20, 'Must be 20 characters or less')
+    .required('Required'),
+  email: yup.string().email("Please enter a valid email").required("Required"),
+  age: yup.number().positive().integer().required("Required"),
+  password: yup
+    .string()
+    .min(5)
+    .matches(passwordRules, { message: "Please create a stronger password" })
+    .required("Required"),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref("password"), null], "Passwords must match")
+    .required("Required"),
+});
+
+export const advancedSchema = yup.object().shape({
+  username: yup
+    .string()
+    .min(3, "Username must be at least 3 characters long")
+    .required("Required"),
+  jobType: yup
+    .string()
+    .oneOf(["designer", "developer", "manager", "other"], "Invalid Job Type")
+    .required("Required"),
+  acceptedTos: yup
+    .boolean()
+    .oneOf([true], "Please accept the terms of service"),
+});
